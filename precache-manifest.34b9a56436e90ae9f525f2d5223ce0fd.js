c5a600e4f9e9fe64d441caee60d9edb4a9879cf5
henry neo <neohenry@gmail.com>
2020-05-25 01:32:52 +0800

    Updates

---
+++ precache-manifest.34b9a56436e90ae9f525f2d5223ce0fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c81ee23895e73dd0527459f40fa364f",
+    "url": "/datatable-app/index.html"
+  },
+  {
+    "revision": "7aac31ecaa1defd53088",
+    "url": "/datatable-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "304da5027189f78eb876",
+    "url": "/datatable-app/static/js/2.ceea6fa4.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/datatable-app/static/js/2.ceea6fa4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7aac31ecaa1defd53088",
+    "url": "/datatable-app/static/js/main.6f044247.chunk.js"
+  },
+  {
+    "revision": "d611e9d340b91d5e876a",
+    "url": "/datatable-app/static/js/runtime-main.d1c7d087.js"
+  }
+]);
