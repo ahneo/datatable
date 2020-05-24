564018da4063b2fd6b1d3c3b9830a436ceffce3e
henry neo <neohenry@gmail.com>
2020-05-24 23:30:22 +0800

    add DataTable

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,21 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import data from "./data/test.json";
 import Button from "@material-ui/core/Button";
 import SimpleTable from "./components/SimpleTable";
+import DataTable from "./components/DataTable";
+import { Container } from "@material-ui/core";
 
 function App() {
   return (
     <React.Fragment>
       <CssBaseline />
-      <SimpleTable />
-      <div>
-        {data.map((element) => {
-          return <div>{element._id}</div>;
-        })}
-      </div>
+      <Container maxWidth="md">
+        <DataTable />
+        <div>
+          {data.map((element) => {
+            return <div>{element._id}</div>;
+          })}
+        </div>
+      </Container>
     </React.Fragment>
   );
   // return (
